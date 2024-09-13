eeeea93c69d409d5018a78113b37d7cf5697b0b1
mayaif <margareta.ifrim@outlook.com>
2024-09-13 15:22:36 +0100

    clean up folders

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+/** @format */
+
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ["expo", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
